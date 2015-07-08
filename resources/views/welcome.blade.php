d902ae49e04cd4190b95aff10b7f1e59b4c42923
Andrew Rodriguez <andrew.rodriguez007@gmail.com>
2015-07-08 11:33:39 -0500

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,7 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
+                height: 100%
                 color: #B0BEC5;
                 display: table;
                 font-weight: 100;
